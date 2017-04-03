4add7181c85eda977b38edacf08b2fda1a5dfdbb
Oksana Kondratiuk <ksyu@web.de>
2017-04-03 14:52:08 +0200

    add drop-down list, get array 'services' and obj 'person'
    get info for back-end

--- input_form/script.js
+++ input_form/script.js
@@ -22,50 +22,68 @@ var inputValue = document.getElementsByTagName("input");
             person[i] = inputValue[i].value;
         }
     }
+// array of services that were chosen by customer
+    var services = [];
+    var o = document.getElementsByClassName('selected-options');
+    for (j=0; j<o.length; j++) {
+        services.push(o[j].textContent);
+    }
     formResults.innerHTML = 'Name: ' + person.fname +"<br>"+ 'Email: ' + person.femail +"<br>"+ 'Title: ' + person.title +"<br>"+ 'Country: '+
-                            person.fcountry +"<br>"+'Region: '+ person.region;
-    formResults.style.color =  '#0b0962';
+                            person.fcountry +"<br>"+'Region: '+ person.region + "<br>"+ 'Chosen services: ' + services;
+    formResults.style.color =  '#607d8b';
+
+
+    console.log('person:', person);
+    console.log('services:', services);
 }
 
 // when drop-down option is selected, how its name before submit button
 var selectBox = document.getElementById('variants');
 
-    // function chooseOption() {
-    //     var element = document.createElement("p");
-    //     var elContent = document.createTextNode(selectBox.options[selectBox.selectedIndex].innerHTML);
-    //     element.appendChild(elContent);
-    //
-    //     var option = document.getElementById('optionChosen');
-    //     option.style.display = 'inline';
-    //      option.insertBefore(element, option.childNodes[0]);
-    //     console.log(elContent)
-    // }
-
     function chooseOption() {
         function insertAfter(referenceNode, newNode) {
                 referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
             }
-        var value = selectBox.options[selectBox.selectedIndex];
+var value = selectBox.options[selectBox.selectedIndex];
 
          if (value.chosen) {
              return true;
          }
         value.chosen ? value.style.display = 'block' : value.style.display = 'none';
-
-        var el = document.createElement("span");
-        el.innerHTML = value.innerHTML;
+// create div
+        var el = document.createElement("div");
+        el.innerHTML = '';
+        el.className = 'set-option'
         var div = document.getElementById("optionChosen");
-        insertAfter(div, el);
         div.style.display = 'block';
+        insertAfter(div, el);
 
+// create span as a click button
         var close = document.createElement("span");
         close.innerHTML = 'X';
+        close.setAttribute('style', 'padding-left: 25px; color: red; cursor: pointer');
+        close.className = 'close-symbol';
         insertAfter(el, close);
+        close.onclick = function () {
+            this.parentElement.removeChild(this);
+            optionText.remove();
+            el.remove();
+            value.style.display = 'block';
 
-        if (close.isclose) {
-            return true;
-        }
-        close.isclose ? value.style.display = 'none' : value.style.display = 'block';
+            // if there is any chosen elements hide p =>'Chosen services:'
+            if(document.getElementsByClassName('set-option').length === 0) {
+                div.style.display = 'none';
+            }
+        };
 
-        console.log(value)
-    }
+
+// create span with option value
+        var optionText = document.createElement("span");
+        optionText.innerHTML = value.innerHTML;
+        optionText.className = 'selected-options'
+        insertAfter(el, optionText);
+}
+// option 'select' is not active
+var optionSelect = selectBox.querySelector('option[value="select"]');
+optionSelect.disabled = 'disabled';
+optionSelect.style.color = '#c2bfbf';
