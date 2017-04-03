ed7c5fb998a9b7019645e189b4915b6674be704c
Oksana Kondratiuk <ksyu@web.de>
2017-04-03 17:43:15 +0200

    add textarea field, body BG, form-container

--- input_form/script.js
+++ input_form/script.js
@@ -1,21 +1,27 @@
 // fill out a form
 var formResults = document.getElementById('results');
-var person = {fname: '', femail: '', title: '', fcountry: '', region: ''};
+var person = {fname: '', femail: '', title: '', country: '', region: ''};
 var inputValue = document.getElementsByTagName("input");
   function validateForm() {
-      // required 'name', 'email' and 'country'
-    // for (x=0; x<inputValue.length; x++) {
-    //     reqName = inputValue[x].getAttribute("name");
-    //     if (reqName.indexOf("f") == 0 ) {
-    //         var requiredField = inputValue[x].value;
-    //         if (requiredField == '') {
-    //             formResults.innerHTML = 'Name, Email and Country must be filled out';
-    //             formResults.style.color =  'red';
-    //             return false;
-    //         }
-    //     }
-    //
-    // }
+     // textarea field
+    var customerText = document.querySelector('textarea').value;
+    //   required 'name', 'email' and 'text'
+    for (x = 0; x < inputValue.length; x++) {
+        reqName = inputValue[x].getAttribute("name");
+        if (reqName.indexOf("f") == 0) {
+            var requiredField = inputValue[x].value;
+            if (requiredField == '') {
+                formResults.innerHTML = 'Name and Email must be filled out';
+                formResults.style.color =  'red';
+                return false;
+            }
+            if( customerText === '') {
+                   formResults.innerHTML = 'Text must be filled out';
+                   formResults.style.color =  'red';
+                   return false;
+               }
+           }
+       }
 
     for (i = 0; i < inputValue.length; i++) {
         for (var i in person) {
@@ -29,11 +35,12 @@ var inputValue = document.getElementsByTagName("input");
         services.push(o[j].textContent);
     }
     formResults.innerHTML = 'Name: ' + person.fname +"<br>"+ 'Email: ' + person.femail +"<br>"+ 'Title: ' + person.title +"<br>"+ 'Country: '+
-                            person.fcountry +"<br>"+'Region: '+ person.region + "<br>"+ 'Chosen services: ' + services;
-    formResults.style.color =  '#607d8b';
-
+                            person.country +"<br>"+'Region: ' + person.region + "<br>"+ 'Your text: ' + customerText + "<br>"+ 'Chosen services: ' + services;
+    formResults.style.color =  '#0e3f1a';
+    changeformBGHeight();
 
     console.log('person:', person);
+    console.log('customerText:', customerText);
     console.log('services:', services);
 }
 
@@ -58,10 +65,12 @@ var value = selectBox.options[selectBox.selectedIndex];
         div.style.display = 'block';
         insertAfter(div, el);
 
+changeformBGHeight();
+
 // create span as a click button
         var close = document.createElement("span");
         close.innerHTML = 'X';
-        close.setAttribute('style', 'padding-left: 25px; color: red; cursor: pointer');
+        close.setAttribute('style', 'padding-left: 25px; color: #d41a0d; cursor: pointer');
         close.className = 'close-symbol';
         insertAfter(el, close);
         close.onclick = function () {
@@ -74,8 +83,8 @@ var value = selectBox.options[selectBox.selectedIndex];
             if(document.getElementsByClassName('set-option').length === 0) {
                 div.style.display = 'none';
             }
+            changeformBGHeight();
         };
-
 
 // create span with option value
         var optionText = document.createElement("span");
@@ -87,3 +96,10 @@ var value = selectBox.options[selectBox.selectedIndex];
 var optionSelect = selectBox.querySelector('option[value="select"]');
 optionSelect.disabled = 'disabled';
 optionSelect.style.color = '#c2bfbf';
+
+// make background container the same height as the form;
+function changeformBGHeight() {
+    var formHeight = document.getElementById('formContent').offsetHeight;
+    document.getElementById('formBG').style.height = formHeight + 'px';
+    console.log('formHeight:', formHeight)
+}
